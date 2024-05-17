f117c5eaecde4777ccd716f3719350965c9196d7
Oscar Escudero <oescudero@erdos-dssp.tsc.urjc.es>
2024-05-17 18:39:16 +0200

    update repo

--- 0_Data/splits_generation.py
+++ 0_Data/splits_generation.py
@@ -1,29 +1,31 @@
 import pandas as pd
 import numpy as np
-
+ 
 import warnings
 warnings.filterwarnings("ignore")
-
+ 
 import pyten
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-
+ 
 import utils
-
-
+ 
+ 
 def preprocessing(params, sep_def, debug=False):
-
+ 
     # Load data
     df = pd.read_parquet(params['path'])
     print("# of patients:", len(df.stay_id.unique()))
     df = utils.get_SI(df, sep_def['Nts_pre'], sep_def['Nts_post'])
     df = utils.get_sep(df, sep_def['N_prog_sep'],  sep_def['increm_sofa'])
     df = df.drop(['diff'], axis=1)
-
+    
+    print(df['sep_12'].isna().sum())
+ 
     # Step -1. Select data of ICU
     df_icu_entry = df[df.stay_time >= 0].reset_index(drop=True)
     print("# of icu-patients:", len(df_icu_entry.stay_id.unique()))
-
+ 
     if debug:
         print("% of missing values (pre-filter):", np.round((df.isnull().sum().sum() / \
                                                              (df.shape[0]*df.shape[1])*100), 4))
@@ -32,38 +34,35 @@ def preprocessing(params, sep_def, debug=False):
         print("# of patients labels:", len(df_icu_entry.stay_id.unique()))
         print("% of missing values post remove patients with nan label:", np.round((df_icu_entry.isnull().sum().sum() / \
                                                              (df_icu_entry.shape[0]*df_icu_entry.shape[1])*100), 4))
-
+ 
     # ## Step 1. Select relevant features. Remove feature without data, based on threshold.
     df_final = df_icu_entry[params['keys']]
-    
     missing_percentage = df_final.isnull().mean() * 100
     feats = missing_percentage[missing_percentage > params['thr_nan']].index.tolist()
-
+ 
     if debug:
         print("Features with more than " + str(params['thr_nan']) + "% missing data:" +  str(len(feats)))
         print("# of patients:", len(df_final.stay_id.unique()))
         print("Dimensiones of dataset:", df_final.shape)
-
+ 
     df_filter = df_final.drop(feats, axis=1)
     df_final = df_filter.astype(float)
-
+ 
     print("Dimensions post remove some feautures:", df_final.shape)
-
+ 
     if params['imputationType'] == "LVCF":
         df_final = utils.LVCF(df_final)
         df_final = df_final.fillna(0)
         print("# of patients post imputation:", len(df_final.stay_id.unique()))
-        
-        
+
         # Calculate the minimum number of timeSteps for any patient in the dataset
+    print(params['min_length_pat'])
     if params['min_length_pat'] == 0:
         min_length_pat = df_final.groupby("stay_id").size().reset_index()[0].min()
-        
     print(min_length_pat)
     params['min_length_pat'] = min_length_pat
-    
     df_final.stay_id = df_final.stay_id.astype(int)
-
+ 
     if debug:
         print("# of patients:", len(df_final.stay_id.unique()))
         print("# of pacientes labeled with 1", len(df_final[df_final.sep_onset == 1][['stay_id']].stay_id.unique()))
@@ -75,10 +74,9 @@ def preprocessing(params, sep_def, debug=False):
     df_filter = utils.filter_windows(df_final, params['w_pre_onset'], params['w_post_onset'])
     if debug:
         print("Dimensiones post-filtering window:", df_filter.shape)
-    
     return df_filter
-
-
+ 
+ 
 def get_tr_te(df, params, seed, debug=True):
     '''
     Rules: 
@@ -87,53 +85,52 @@ def get_tr_te(df, params, seed, debug=True):
         - We will allocate 80% for training and 20% for testing.
         - We won't balance the datasets.
     '''
-
+ 
     # Split the patients into training and testing sets
     train_stay_ids, test_stay_ids = \
         train_test_split(df.stay_id.unique(), 
                          test_size=0.2, 
                          random_state=seed)
-
+ 
     # Filter the DataFrame based on the stay_ids in the training and testing sets
     train_df = df[df['stay_id'].isin(train_stay_ids)].reset_index(drop=True)
     print(train_df.keys())
     y_train_df = train_df[params['f_tr_te']].reset_index(drop=True)
     train_df = train_df.drop(params['label'], axis=1)
-
+    print(y_train_df['sep_12'].isna().sum())
+ 
     test_df = df[df['stay_id'].isin(test_stay_ids)].reset_index(drop=True)
     y_test_df = test_df[params['f_tr_te']].reset_index(drop=True)
     test_df = test_df.drop(params['label'], axis=1)
 
+ 
     if debug:
         # Print the number of patients in the training and testing sets
         print("# of windowing patients (train):", len(train_df.w_id.unique()), "- # of original patients:", len(train_df.stay_id.unique()))
         print("# of windowing patients (test):", len(test_df.w_id.unique()), "- # of original patients:", len(test_df.stay_id.unique()))
-        
     # Normalization
     scaler = StandardScaler()
     scaler.fit(train_df[list(train_df.keys())[2:-1]])
     X_train_scaled = scaler.transform(train_df[list(train_df.keys())[2:-1]])
     X_train_scaled = pd.DataFrame(X_train_scaled, columns=train_df.columns[2:-1])
     X_train_scaled = pd.concat([X_train_scaled, train_df[['stay_id', 'stay_time', 'w_id']]], axis=1)
-
+ 
     X_test_scaled = scaler.transform(test_df[list(test_df.keys())[2:-1]])
     X_test_scaled = pd.DataFrame(X_test_scaled, columns=test_df.columns[2:-1])
     X_test_scaled = pd.concat([X_test_scaled, test_df[['stay_id', 'stay_time', 'w_id']]], axis=1)
-
+ 
     # Convert the DataFrames into tensors while discarding the first two and last features
     X_train = utils.dataframe_to_tensor(X_train_scaled, params['min_length_pat'], 'w_id')
     X_train = X_train[:, :, 0:X_train.shape[2]-3]
     X_test = utils.dataframe_to_tensor(X_test_scaled, params['min_length_pat'], 'w_id')
     X_test = X_test[:, :, 0:X_test.shape[2]-3]
-
+    
+    print("==>", y_test_df['sep_12'].isna().sum())
+    print(y_test_df.shape)
+    print(params['min_length_pat'])
+ 
     y_train = utils.dataframe_to_tensor(y_train_df, params['min_length_pat'], 'w_id')
     y_train = y_train[:, :, -1]
     y_test = utils.dataframe_to_tensor(y_test_df, params['min_length_pat'], 'w_id')
     y_test = y_test[:, :, -1]
-    
     return  X_train, X_test, y_train, y_test
-
-
-       
-    
-    
--- 0_Data/utils.py
+++ 0_Data/utils.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-
-
-def dataframeToTensor(df, timeStepLength, mainKey):
+ 
+ 
+def dataframe_to_tensor(df, timeStepLength, mainKey):
     """
     Convert a pandas DataFrame into a numpy tensor for time series analysis.
-
+ 
     Args:
     - df: pandas DataFrame, which should contain data for multiple time steps per patient.
     - timeStepLength: Desired length of the time step sequence.
-
+ 
     Returns:
     - X: Numpy tensor with dimensions (number of patients, timeStepLength, number of features).
     """
@@ -18,10 +17,8 @@ def dataframeToTensor(df, timeStepLength, mainKey):
     X = []
     for patient in listPatients:
         df_patient = df[df[mainKey] == patient]
-        
         # Drop any rows with missing 'timeStep'
         df_patient = df_patient.dropna(subset=['stay_time'])
-        
         if len(df_patient) >= timeStepLength:
             X_patient = df_patient.iloc[:timeStepLength].values
         else:
@@ -29,78 +26,75 @@ def dataframeToTensor(df, timeStepLength, mainKey):
             padding = np.full((timeStepLength - len(df_patient), df.shape[1]), np.nan)
             X_patient = np.vstack([df_patient.values, padding])
         X.append(X_patient)
-
+ 
     return np.array(X, dtype="float")
-
+ 
 def LVCF(df):
-    
     df = df.sort_values(by=['stay_id', 'stay_time'])
     for patient in df['stay_id'].unique():
         last_value = df[df['stay_id'] == patient].fillna(method='ffill').iloc[-1]
         df.loc[df['stay_id'] == patient, df.columns[2:]] = df.loc[df['stay_id'] == patient, df.columns[2:]].fillna(last_value)
-    
     return df
-
-
-def removeIncompleteWindows(data, min_length_patient):
-    repetitions = data['stay_id_split'].value_counts()
-    idxs = repetitions[repetitions != min_length_patient].index
-    idxs_to_remove = data[data['stay_id_split'].isin(idxs)].index
-    data = data.drop(idxs_to_remove).reset_index(drop=True)
-    return data
-
-
-def getTrTe(df, seed, debug=True):
-    '''
-    Rules: 
-        - Split the same patients into windows. For them, we define a window length as a parameter.
-        - The same patient with all windows can only be included in a single split (either train or test).
-        - We will allocate 80% for training and 20% for testing.
-        - We won't balance the datasets.
-    '''
-    # Calculate the minimum number of timeSteps for any patient in the dataset
-    min_length_patient = df.groupby("stay_id").size().reset_index()[0].min()
-
-    # Make a copy of the original DataFrame to avoid modifying the original data
-    df.stay_id = df.stay_id.astype(int)
-
-    # Assign a sequential count within each group (patient) in the 'stay_id' column to a new column 'row_per_pat'
-    df['row_per_pat'] = df.groupby('stay_id').cumcount()
-    # Divide patients into groups of 4 rows and assign a 'split' number accordingly
-    df['split'] = (df['row_per_pat'] // min_length_patient) + 1
-
-    # Create a new column 'stay_id_split' by concatenating 'stay_id' and 'split' columns
-    df['stay_id_split'] = df['stay_id'].astype(str) + '_' + df['split'].astype(str)
-    # Drop the auxiliary columns 'row_per_pat' and 'split'
-    df = df.drop(['row_per_pat', 'split'], axis=1)
-    # Sort the DataFrame by 'stay_id' column
-    df_sorted = df.sort_values('stay_id')
-
-    # Group the DataFrame by 'stay_id' and aggregate the values into lists
-    df_grouped = df_sorted.groupby('stay_id').agg(lambda x: x.tolist()).reset_index()
-    df_grouped.reset_index(drop=True, inplace=True)
-
-    # Split the patients into training and testing sets
-    train_stay_ids, test_stay_ids = \
-        train_test_split(df_grouped['stay_id'], 
-                         test_size=0.2, 
-                         random_state=seed)
-
-    # Filter the DataFrame based on the stay_ids in the training and testing sets
-    train_df = df[df['stay_id'].isin(train_stay_ids)].reset_index(drop=True)
-    train_df = removeIncompleteWindows(train_df, min_length_patient)
-    y_train_df = train_df[['stay_id', 'stay_time', 'stay_id_split', 'sepshk_der']].reset_index(drop=True)
-    train_df = train_df.drop(['sepshk_der'], axis=1)
-
-    test_df = df[df['stay_id'].isin(test_stay_ids)].reset_index(drop=True)
-    test_df = removeIncompleteWindows(test_df, min_length_patient)
-    y_test_df = test_df[['stay_id', 'stay_time', 'stay_id_split', 'sepshk_der']].reset_index(drop=True)
-    test_df = test_df.drop(['sepshk_der'], axis=1)
-    
-    if debug:
-        print("Length of window:", min_length_patient)
-        # Print the number of patients in the training and testing sets
-        print("# of windowing patients (train):", len(train_df.stay_id_split.unique()), "- # of original patients:", len(train_df.stay_id.unique()))
-        print("# of windowing patients (test):", len(test_df.stay_id_split.unique()), "- # of original patients:", len(test_df.stay_id.unique()))
-
-    return train_df, y_train_df, test_df, y_test_df, min_length_patient
+ 
+ 
+def slidingWindow(df, moving_span, window_length):
+ 
+    list_patients = np.unique(df.stay_id)
+    df_sw_ttl = pd.DataFrame()
+    for idx_pat in range(len(list_patients)):
+        df_sw = pd.DataFrame()
+        df_patient = df[df.stay_id == list_patients[idx_pat]]
+        id_pat = df_patient.stay_id.unique()[0]
+        iterations = int(np.ceil((df_patient.shape[0] - (window_length)+1) / moving_span))
+        for j in range(iterations):
+            df_aux = df_patient[moving_span*j: (window_length + moving_span*j)]
+            print(df_aux.shape)
+            df_aux['w_id'] = str(id_pat) + "_" + str(j)
+            df_sw = pd.concat([df_sw, df_aux],ignore_index=True)
+ 
+        df_sw_ttl = pd.concat([df_sw_ttl, df_sw],ignore_index=True)
+ 
+    return df_sw_ttl
+ 
+ 
+def filter_windows(df, N, M):
+    w = []
+    for stay_id, group in df.groupby('stay_id'):
+        group = group.reset_index(drop=True)
+        sep_onset_indices = group.index[group['sep_onset'] == 1].tolist()
+        for idx in sep_onset_indices:
+            start_idx = max(idx - N, 0)
+            end_idx = min(idx + M + 1, len(group))
+            w.append(group.iloc[start_idx:end_idx])
+    return pd.concat(w, ignore_index=True)
+ 
+ 
+def get_SI(df, Nts_pre, Nts_post):
+    df['SI'] = 0
+    pats = list(df.stay_id.unique())
+    for i in range(len(pats)):
+        data = df[df.stay_id == pats[i]]
+        try:
+            true_index = data[data['abx'] == True].index[0]
+            start_index = max(0, true_index - Nts_pre)
+            end_index = min(len(df), true_index + Nts_post + 1)
+            df.loc[start_index:end_index, 'SI'] = 1
+        except IndexError:
+            continue
+    return df
+ 
+def prop_sep(df, n_prog):
+    df['sep_'+str(n_prog)] = 0
+    for pat, grupo in df.groupby('stay_id'):
+        sep_onset_indices = grupo.index[grupo['sep_onset'] == 1]
+        for index in sep_onset_indices:
+            df.loc[index:index+n_prog, 'sep_'+str(n_prog)] = 1
+    return df
+ 
+ 
+def get_sep(df, N_prog_sep, increm):
+ 
+    df['diff'] = df.groupby('stay_id')['sofa'].diff()
+    df['sep_onset'] = ((df['diff'] >= increm) & (df['SI'] == True)).astype(int)
+    df = prop_sep(df, N_prog_sep)
+    return df
