67efed00f9b7bc7ac0d77b8ecf78ada00fec8a44
bahp <bernard.hernandez.perez@gmail.com>
2024-05-30 21:37:00 +0100

    - removed

---
+++ experiments/utils/scores.py
@@ -0,0 +1,34 @@
+"""
+Implement any score if needed.
+
+"""
+def gcs():
+    """"""
+    pass
+
+
+def qsofa(sbp, rr, gcs):
+    """Computes the quick Sofa score.
+
+    Parameters
+    ----------
+    sbp:
+        Systolic blood pressure (mmHg)
+    rr:
+        Respiratory rate (breaths/min)
+    gcs:
+        Glasgow comma score
+    """
+    pass
+
+
+
+def sofa():
+    """
+
+    Ref: https://en.wikipedia.org/wiki/SOFA_score
+
+    Parameters
+    ----------
+    """
+    pass
